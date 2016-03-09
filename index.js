35ef8eb9b337152b699a7ca21b2570a434a189d8
Craig Hammell <cilphex@gmail.com>
2016-03-09 11:23:12 -0800

    small things

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var Config = require('./lib/configuration');
 var Limit = require('./lib/limit');
+var LimitGroup = require('./lib/limit_group');
 
 module.exports = {
   Config: Config,
   Limit: Limit,
+  LimitGroup: LimitGroup
 }
---
+++ tests/limit_group.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+var async = require('async');
+var timekeeper = require('timekeeper');
+
+var TrafficJam = require('../index');
+
+describe('stuff', function() {
+  it('should do something', function(done) {
+    done();
+  });
+});
